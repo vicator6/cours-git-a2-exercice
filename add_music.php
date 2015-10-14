2730bdde625d7b521a85958d31360d7494f49042
vicator6 <Victor@MacBook-Pro-3.local>
2015-10-14 18:52:08 +0200

    Fix #3

--- add_music.php
+++ add_music.php
@@ -14,25 +14,25 @@ if( isset($_FILES['music']) && !empty($_FILES['music']) &&
 
 
 
-        $ext = strtolower(substr(strrchr($file['name'], '.')  ,1));
-        // Vérification des extentions
-        if (preg_match('/\.(mp3|ogg)$/i', $file['name'])) {
-            $filename = md5(uniqid(rand(), true));
-            $destination = "musics/{$filename}.{$_SESSION['id']}.{$ext}";
-            move_uploaded_file($filename, $destination);
+    $ext = strtolower(substr(strrchr($file['name'], '.')  ,1));
+    // Vérification des extentions
+    if (preg_match('/\.(mp3|ogg)$/i', $file['name'])) {
+        $filename = md5(uniqid(rand(), true));
+        $destination = "musics/{$filename}.{$_SESSION['id']}.{$ext}";
+        move_uploaded_file($filename, $destination);
 
-            $add = addMusic($db, $_GET['id'], $_POST['title'], $_SESSION['id']);
+        $add = addMusic($db, $_GET['id'], $_POST['title'], $_SESSION['id']);
 
-            if($add== true){
-                header('Location: dashboard.php');
-            }
-            else{
-                $error= 'Rater!';
-            }
-        } else {
-            $error = 'Erreur, le fichier n\'a pas une extension autorisée !';
+        if($add== true){
+            header('Location: dashboard.php');
         }
+        else{
+            $error= 'Rater!';
+        }
+    } else {
+        $error = 'Erreur, le fichier n\'a pas une extension autorisée !';
     }
+}
 }
 include 'view/_header.php';
 include 'view/add_music.php';
